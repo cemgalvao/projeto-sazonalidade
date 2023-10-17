35265cc8f19206fba43e089b0715d6f3687a6e24
BiaBaccaro <130668481+BiaBaccaro@users.noreply.github.com>
2023-10-17 14:52:56 -0300

    primeiro fase do trabalho

---
+++ sazonalidade.py
@@ -0,0 +1,13 @@
+dia = int(input('digite um dia'))
+mês = int(input('digite um mês'))
+
+if  dia == 1 or dia ==2 or dia == 3:
+    print('verão')
+elif dia == 4 or dia ==5 or dia == 6:
+    print('outono')
+elif dia == 7 or dia ==8 or dia == 9:
+    print('inverno')
+elif dia == 10 or dia ==11 or dia == 12:
+    print('primavera')
+else:
+    print('escola um número que preste')
